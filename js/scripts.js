a02c2c4953480b3f28a3649eb879aa3e60c757f3
nealwaga <waganeal@gmail.com>
2022-03-13 15:50:36 +0300

    product development

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $("#design").click(function(){
     $("#p").toggle();
 });
+
 $("#development").click(function(){
     $("#b").toggle();
+});
+$("#productDevelopment").click(function(){
+    $("#r").toggle();
 });
